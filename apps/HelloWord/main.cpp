1571464c6e794433c113c239dae072a9b6cec25e
Nicolas Gourdier <nyquo.g@gmail.com>
2024-07-29 11:32:41 +0200

    Create hello word window

--- apps/HelloWord/main.cpp
+++ apps/HelloWord/main.cpp
@@ -1,9 +1,50 @@
-#include <iostream>
+#include <gl.h>
 
 #include <Logger.hpp>
+
+bool continueRunning{true};
+
+void windowCloseCallback(GLFWwindow* window) {
+    continueRunning = false;
+}
 
 int main(int argc, char** argv) {
 	core::Logger::setLogLevel(core::Logger::LogLevel::Info);
 
 	core::Logger::logInfo("Program started!");
+
+    if (!glfwInit())
+    {
+        core::Logger::logError("Failed to init GLFW!");
+        throw(std::runtime_error("Failed to init GLFW!"));
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    auto window = glfwCreateWindow(800, 600, "Hello word!", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        core::Logger::logError("Failed to initialize GLAD");
+        throw(std::runtime_error("Failed to initialize GLAD"));
+    }
+    std::atomic_bool {true};
+
+    glfwSetWindowCloseCallback(window, windowCloseCallback);
+
+    while(continueRunning) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
 }
