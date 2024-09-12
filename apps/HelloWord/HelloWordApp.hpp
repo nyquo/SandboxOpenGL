9a4db4a0e03f33ae86e86b5f59c263bb945214d6
Nicolas GOURDIER <nyquo.g@gmail.com>
2024-09-12 17:13:52 +0200

    Broken Bao.jpg

--- apps/HelloWord/HelloWordApp.hpp
+++ apps/HelloWord/HelloWordApp.hpp
@@ -5,6 +5,7 @@
 #include <Layer.hpp>
 #include <Logger.hpp>
 #include <Shader.hpp>
+#include <stb_image.h>
 
 #include <cmath>
 
@@ -17,18 +18,46 @@ class CustomLayer : public core::Layer
 
         glGenBuffers(1, &VBO);
         glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &EBO);
 
         glBindVertexArray(VAO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
         // positions
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
 
         // colors
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
         glEnableVertexAttribArray(1);
+
+        // textures
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
+
+        int width, height, channels;
+        unsigned char* data = stbi_load("assets/BUI.jpg", &width, &height, &channels, 0);
+
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // set the texture wrapping/filtering options (on the currently bound texture object)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        if(data)
+        {
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+        }
+
+        stbi_image_free(data);
 
         glBindVertexArray(0);
     };
@@ -37,6 +66,7 @@ class CustomLayer : public core::Layer
     {
         glDeleteVertexArrays(1, &VAO);
         glDeleteBuffers(1, &VBO);
+        glDeleteBuffers(1, &EBO);
     }
 
     void onUpdate() override
@@ -51,16 +81,30 @@ class CustomLayer : public core::Layer
 
         shaderProgram->setFloat("offset", offset);
 
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     }
 
     void onEvent(core::Event& event) override {}
 
   private:
-    float vertices[18]{
-      -0.5F, -0.5F, 0.0F, 1.0F, 0.0F, 0.0F, 0.0F, 0.5F, 0.0F, 0.0F, 1.0F, 0.0F, 0.5F, -0.5F, 0.0F, 0.0F, 0.0F, 1.0F};
-    unsigned int VBO, VAO;
+    float vertices[36]{
+      // positions        // colors         // texture coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+    };
+    unsigned int indices[6] = {
+      0,
+      1,
+      3, // first triangle
+      1,
+      2,
+      3 // second triangle
+    };
+    unsigned int VBO, VAO, EBO, texture;
     std::unique_ptr<core::Shader> shaderProgram;
 };
 
--- apps/HelloWord/shaders/BasicShader.frag
+++ apps/HelloWord/shaders/BasicShader.frag
@@ -1,11 +1,12 @@
 #version 330 core
 in vec3 ourColor;
-in vec3 vertexPosition;
+in vec2 texCoord;
 
 out vec4 FragColor;
 
+uniform sampler2D ourTexture;
+
 void main()
 {
-    FragColor = vec4(abs(vertexPosition.x), abs(vertexPosition.y), vertexPosition.z, 1.0);
-   // FragColor = vec4(ourColor, 1.0);
+    FragColor = texture(ourTexture, texCoord);
 }
