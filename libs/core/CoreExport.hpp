77dc7131d6e4f8ccb96442d6edaa93d04e4af3a1
Nicolas Gourdier <nyquo.g@gmail.com>
2024-09-23 11:07:34 +0200

    Temporary cmake fix

--- libs/core/CoreExport.hpp
+++ libs/core/CoreExport.hpp
@@ -1,11 +1,28 @@
 #pragma once
 
+//#ifndef CORE_API
+//	#ifdef CORE_API_EXPORT
+//		#ifdef _WIN32
+//			#define CORE_API __declspec(dllexport)
+//		#else
+//			#define CORE_API
+//		#endif
+//	#else
+//		#ifdef _WIN32
+//			#define CORE_API __declspec(dllimport)
+//		#else
+//			#define CORE_API
+//		#endif
+//	#endif
+//#endif
+
 #ifdef _WIN32
-	#ifdef BUILDING_DLL
+	#define CORE_API
+	/*#ifdef CORE_API_EXPORT
 		#define CORE_API __declspec(dllexport)
 	#else
 		#define CORE_API __declspec(dllimport)
-	#endif
+	#endif*/
 #else
 	#define CORE_API
 #endif
