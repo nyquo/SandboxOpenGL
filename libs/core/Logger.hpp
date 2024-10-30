4af04a8537ec0a2df6060bbb48471371d13dd047
Nicolas GOURDIER <nyquo.g@gmail.com>
2024-10-30 13:50:12 +0100

    Variadic logger

--- libs/core/Logger.hpp
+++ libs/core/Logger.hpp
@@ -21,39 +21,39 @@ class CORE_API Logger
 
     static void setLogLevel(LogLevel logLevel) { s_logLevel = logLevel; }
 
-    template<typename T>
-    static void logError(const T& error)
+    template<typename... Ts>
+    static void logError(const Ts&... error)
     {
-        log(LogLevel::Error, error);
+        log(LogLevel::Error, error...);
     }
 
-    template<typename T>
-    static void logWarning(const T& warning)
+    template<typename... Ts>
+    static void logWarning(const Ts&... warning)
     {
-        log(LogLevel::Warning, warning);
+        log(LogLevel::Warning, warning...);
     }
 
-    template<typename T>
-    static void logInfo(const T& info)
+    template<typename... Ts>
+    static void logInfo(const Ts&... info)
     {
-        log(LogLevel::Info, info);
+        log(LogLevel::Info, info...);
     }
 
-    template<typename T>
-    static void logDebug(const T& debug)
+    template<typename... Ts>
+    static void logDebug(const Ts&... debug)
     {
-        log(LogLevel::Debug, debug);
+        log(LogLevel::Debug, debug...);
     }
 
-    template<typename T>
-    static void logTrace(const T& trace)
+    template<typename... Ts>
+    static void logTrace(const Ts&... trace)
     {
-        log(LogLevel::Trace, trace);
+        log(LogLevel::Trace, trace...);
     }
 
   private:
-    template<typename T>
-    static void log(LogLevel level, const T& t)
+    template<typename... Ts>
+    static void log(LogLevel level, const Ts&... t)
     {
         if(s_logLevel < level)
         {
@@ -62,7 +62,8 @@ class CORE_API Logger
         std::time_t time = std::time(nullptr);
         char strTime[50];
         std::strftime(strTime, sizeof(strTime), "%H:%M:%S", std::localtime(&time));
-        std::cout << logLevelToStr(level) << " " << t << std::endl;
+        std::cout << logLevelToStr(level) << " ";
+        (std::cout << ... << t) << std::endl;
     }
 
     static std::string logLevelToStr(LogLevel level)
