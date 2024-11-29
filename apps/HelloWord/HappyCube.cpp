71ed657acb5b6b8fb9b9808298e0bca2c8e83ed6
Nicolas GOURDIER <nyquo.g@gmail.com>
2024-11-29 14:10:57 +0100

    Remove helloword and lighting app

--- apps/HelloWord/HappyCube.cpp
+++
@@ -1,9 +0,0 @@
-#include "HappyCube.hpp"
-
-HappyCube::HappyCube(glm::vec3 position) { m_position = position; }
-
-HappyCube::~HappyCube() {}
-
-const float* HappyCube::getRawVertices() const { return m_vertices.data(); }
-
-size_t HappyCube::getVertexCount() const { return m_vertices.size(); }
