1ad54679b600e5f7f90910989611963f77fa0dd5
Nicolas Gourdier <nyquo.g@gmail.com>
2025-10-29 15:05:55 +0100

    Fix texture destruction

--- libs/renderer/Texture.cpp
+++ libs/renderer/Texture.cpp
@@ -106,6 +106,7 @@ Texture::~Texture()
         m_loadedCount[m_path].first--;
         if(m_loadedCount[m_path].first == 0)
         {
+            m_loadedCount.erase(m_path);
             glDeleteTextures(1, &m_textureData.textureId);
         }
     }
