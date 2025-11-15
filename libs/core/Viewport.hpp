906bdeda702f7c389dadae1316fb3bafa0d90a58
Nicolas Gourdier <nyquo.g@gmail.com>
2025-11-15 19:37:52 +0100

    Add Viewport class in core

---
+++ libs/core/Viewport.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <imgui.h>
+#include <string>
+
+namespace core {
+
+class Viewport
+{
+  public:
+    Viewport(float m_layerWidth,
+             float m_layerHeight,
+             std::string viewportName = "Viewport",
+             float x = 0,
+             float y = 0,
+             float width = 800,
+             float height = 600);
+    ~Viewport();
+
+    float getX() const;
+    float getY() const;
+    float getWidth() const;
+    float getHeight() const;
+
+    void displayViewportWindow();
+    void setLayerSize(float width, float height);
+
+  private:
+    ImVec2 m_vMin{0.0f, 0.0f};
+    ImVec2 m_vMax{800.0f, 600.0f};
+
+    float m_x;
+    float m_y;
+    float m_width;
+    float m_height;
+    float m_layerWidth; // Not really used
+    float m_layerHeight;
+
+    std::string m_viewportName;
+};
+
+}
